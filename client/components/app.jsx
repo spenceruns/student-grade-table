9871e2385da05b56f6f7e88eefbd907a3260c6c0
Spencer Allen <spenceruns@gmail.com>
2020-01-06 22:57:51 +0000

    completed view average score

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,20 +6,36 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      grades: []
+      grades: [],
+      averageGrade: 0
     };
   }
 
   componentDidMount() {
     fetch('/api/grades')
       .then(res => res.json())
-      .then(data => this.setState({ grades: data }));
+      .then(data => this.setState({ grades: data }, () => {
+        this.getAverageGrade();
+      }));
+  }
+
+  getAverageGrade() {
+    let total = 0;
+    let amountOfStudents = 0;
+    let average = 0;
+
+    this.state.grades.forEach(grade => {
+      total += grade.grade;
+      amountOfStudents++;
+    });
+    average = Math.round(total / amountOfStudents);
+    this.setState({ averageGrade: average });
   }
 
   render() {
     return (
       <div>
-        <Header />
+        <Header averageGrade={this.state.averageGrade}/>
         <GradeTable grades={this.state.grades}/>
       </div>
     );
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 function Header(props) {
   return (
     <div className="container">
-      <div className="row">
-        <h1 className="ml-2 mt-4">Student Grade Table</h1>
+      <div className="row d-flex justify-content-between">
+        <h1 className="mt-4">Student Grade Table</h1>
+        <h2 className="mt-4">Average Grade<span className="badge badge-secondary">{props.averageGrade}</span></h2>
       </div>
     </div>
   );
