c158f5d53456060bff8f9fe4a34ba3e512b6d9e5
Spencer Allen <spenceruns@gmail.com>
2020-01-06 23:08:24 +0000

    changed issues

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,36 +6,32 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      grades: [],
-      averageGrade: 0
+      grades: []
     };
   }
 
   componentDidMount() {
     fetch('/api/grades')
       .then(res => res.json())
-      .then(data => this.setState({ grades: data }, () => {
-        this.getAverageGrade();
-      }));
+      .then(data => this.setState({ grades: data }))
+      .catch(err => console.error(err));
   }
 
   getAverageGrade() {
     let total = 0;
     let amountOfStudents = 0;
-    let average = 0;
-
     this.state.grades.forEach(grade => {
       total += grade.grade;
       amountOfStudents++;
     });
-    average = Math.round(total / amountOfStudents);
-    this.setState({ averageGrade: average });
+    return Math.round(total / amountOfStudents);
   }
 
   render() {
+    const average = (this.state.grades.length > 0) ? this.getAverageGrade() : 0;
     return (
       <div>
-        <Header averageGrade={this.state.averageGrade}/>
+        <Header averageGrade={average}/>
         <GradeTable grades={this.state.grades}/>
       </div>
     );
