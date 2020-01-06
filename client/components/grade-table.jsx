75f5918a3e640fd13524c4620bfa9b8dcfa3c262
Spencer Allen <spenceruns@gmail.com>
2020-01-06 20:51:35 +0000

    Completed view all grades

---
+++ client/components/grade-table.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Grade from './grade';
+
+function TableHeading(props) {
+  return (
+    <div className="row border-bottom border-dark bg-secondary">
+      <div className="col h3 mb-0 border-right border-dark">Student Name</div>
+      <div className="col h3 mb-0 border-right border-dark">Course</div>
+      <div className="col h3 mb-0">Grade</div>
+    </div>
+  );
+}
+
+function EmptyTable() {
+  return (
+    <div className="row">
+      <h1 className="mx-auto my-3">No Students Entered</h1>
+    </div>
+  );
+}
+
+function FilledTable(props) {
+  return props.grades.map(grade => {
+    return (
+      <div key={props.grades.id} className="row" >
+        <Grade grade={grade} />
+      </div >
+    );
+  });
+}
+
+class GradeTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEmpty: true
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.grades) {
+      this.setState({ isEmpty: false });
+    }
+  }
+
+  render() {
+    const status = (this.state.isEmpty) ? <EmptyTable /> : <FilledTable grades={this.props.grades}/>;
+
+    return (
+      <div className="container border border-dark">
+        <TableHeading />
+        { status }
+      </div>
+    );
+  }
+}
+
+export default GradeTable;
---
+++ client/components/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header(props) {
+  return (
+    <div className="container">
+      <div className="row">
+        <h1 className="ml-2 mt-4">Student Grade Table</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
